6722e95f554f72e18667bf3e32ddaa0af60d6a8b
minimalDev01 <sergiocn.dev@gmail.com>
2021-04-24 12:00:54 +0200

    Initial Commit

---
+++ FinanceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinanceApp
+//
+//  Created by Sergio Carralero Nuño on 24/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
