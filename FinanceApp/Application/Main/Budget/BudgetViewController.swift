40b2648fa27c24b7888c6db89e7b3205350ebc0b
minimalDev01 <sergiocn.dev@gmail.com>
2021-04-25 11:31:16 +0200

    Preparing Budget storyboard

--- FinanceApp/Application/Main/Budget/BudgetViewController.swift
+++ FinanceApp/Application/Main/Budget/BudgetViewController.swift
@@ -1,29 +1,18 @@
 //
-//  TransactionsViewController.swift
+//  BudgetViewController.swift
 //  FinanceApp
 //
-//  Created by Sergio Carralero Nuño on 24/4/21.
+//  Created by Sergio Carralero Nuño on 25/4/21.
 //
 
 import UIKit
 
-class TransactionsViewController: UIViewController {
+class BudgetViewController: UIViewController {
     // MARK: -IBOutlets
     @IBOutlet weak var tableView: UITableView!
     
-    // MARK: -Properties
-    fileprivate(set) lazy var emptyStateView: UIView = {
-        guard let view = Bundle.main.loadNibNamed("EmptyState", owner: nil, options: [:])?.first as? UIView else {
-            return UIView()
-        }
-        
-        return view
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
         
         let cell = UINib(nibName: "TransactionsCell", bundle: Bundle.main)
         tableView.register(cell, forCellReuseIdentifier: "cell")
@@ -46,16 +35,13 @@ class TransactionsViewController: UIViewController {
 }
 
 // MARK: -Extensions
-extension TransactionsViewController: UITableViewDelegate {
+extension BudgetViewController: UITableViewDelegate {
     
 }
 
-extension TransactionsViewController: UITableViewDataSource {
+extension BudgetViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let count = 10
-        tableView.backgroundView = count == 0 ? emptyStateView : nil
-        tableView.separatorStyle = count == 0 ? .none : .singleLine
-        
         return count
     }
     
